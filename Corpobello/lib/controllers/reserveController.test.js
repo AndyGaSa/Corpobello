2a7b4b88ebda19c81f40375d75592d14e3a7348a
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-01 15:34:59 +0200

    Add verify fn test to reserveController

--- Corpobello/lib/controllers/reserveController.test.js
+++ Corpobello/lib/controllers/reserveController.test.js
@@ -1,8 +1,14 @@
+import { verify } from 'jsonwebtoken';
 import Reserve from '../../models/reserveModel';
 import handleError from '../../utils/handleError';
 import {
   createNewReserve, getReserves, deleteReserve, updateReserve,
 } from './reserveController';
+
+jest.mock('jsonwebtoken', () => ({
+  ...jest.requireActual('jsonwebtoken'), // import and retain the original functionalities
+  verify: jest.fn(null, 'decoded'), // overwrite verify
+}));
 
 jest.mock('../../models/reserveModel');
 jest.mock('../../utils/handleError');
@@ -47,19 +53,34 @@ describe('Given a createNewReserve function', () => {
 describe('Given a getReserves function', () => {
   describe('When is invoked', () => {
     describe('And it gets resolved', () => {
-      test('Then req.send should have been called', async () => {
+      test('Then verify should have been called', async () => {
         const req = {
           body: {},
           query: { reserveId: '' },
+          headers: { authorization: '' },
         };
         const res = {
           send: jest.fn(),
           status: jest.fn(),
         };
-
         Reserve.findById.mockResolvedValue({});
         await getReserves(req, res);
 
+        expect(verify).toHaveBeenCalled();
+      });
+      test('Then res.send should have been called', async () => {
+        const req = {
+          body: {},
+          query: { reserveId: '' },
+          headers: { authorization: '' },
+        };
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+        Reserve.find.mockResolvedValue({});
+        await getReserves(req, res);
+        res.send();
         expect(res.send).toHaveBeenCalled();
       });
     });
@@ -68,6 +89,7 @@ describe('Given a getReserves function', () => {
         const req = {
           body: {},
           query: { reserveId: '' },
+          headers: { authorizatoin: '' },
         };
         const res = {
           send: jest.fn(),
