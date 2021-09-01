43c5251225730f7176ba4d726799fd81bff25cf7
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-01 16:41:44 +0200

    Create a global header with links to all pages

--- Corpobello/components/Header/Header.jsx
+++ Corpobello/components/Header/Header.jsx
@@ -1,9 +1,32 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import bigLogoCb from '../../images/bigLogoCb.svg';
+import './Header.css';
 
 export default function Header() {
   return (
-    <>
-      <h1>Hola esto es un header</h1>
-    </>
+    <header>
+      <Image src={bigLogoCb} alt="Picture of the author" />
+      <Link href="/">
+        <a>INICIO</a>
+      </Link>
+      <Link href="/services">
+        <a>SERVICIOS</a>
+      </Link>
+      <Link href="/reserves">
+        <a>RESERVA</a>
+      </Link>
+      <Link href="/events">
+        <a>EVENTOS</a>
+      </Link>
+      <Link href="/contact">
+        <a>CONTACTO</a>
+      </Link>
+      <Link href="/login">
+        <a>INCIAR SESION</a>
+      </Link>
+    </header>
   );
 }
