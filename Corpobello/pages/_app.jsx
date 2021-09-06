a8bccf00ab8b29054868298c6bf0c3e09a1bccc4
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-06 10:40:32 +0200

    Import moment and configprovider from antd to set language on ES

--- Corpobello/pages/_app.jsx
+++ Corpobello/pages/_app.jsx
@@ -1,15 +1,22 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { ConfigProvider } from 'antd';
+import esES from 'antd/lib/locale/es_ES';
+import moment from 'moment';
+import 'moment/locale/es';
+
 import Header from '../components/Header';
+import 'antd/dist/antd.css';
 import '../styles/Global.css';
 
 function MyApp({ Component, pageProps }) {
+  moment.locale('es');
   return (
-    <>
+    <ConfigProvider locale={esES}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </ConfigProvider>
   );
 }
 
