813770317e111a37e10dfbe61b550c20f4f851a1
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-09 19:25:31 +0200

    Configure provider on next app for redux

--- Corpobello/pages/_app.jsx
+++ Corpobello/pages/_app.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { ConfigProvider } from 'antd';
-import esES from 'antd/lib/locale/es_ES';
 import moment from 'moment';
 import 'moment/locale/es';
 
+import { Provider } from 'react-redux';
+import configureStore from '../redux/stores';
 import Header from '../components/Header';
 import 'antd/dist/antd.css';
 import '../styles/Global.css';
@@ -13,10 +13,10 @@ import '../styles/Global.css';
 function MyApp({ Component, pageProps }) {
   moment.locale('es');
   return (
-    <ConfigProvider locale={esES}>
+    <Provider store={configureStore()}>
       <Header />
       <Component {...pageProps} />
-    </ConfigProvider>
+    </Provider>
   );
 }
 
