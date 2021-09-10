5ae2e55823973b6b03d6111aa81e1d76235c764c
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-10 14:57:33 +0200

    Import reserves modules on reserves page and add title

--- Corpobello/pages/reserves.jsx
+++ Corpobello/pages/reserves.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Reserves from '../components/Reserves';
+import styles from '../styles/Services.module.css';
 
-export default function Reserves() {
+export default function ReservesPage() {
   return (
     <main>
-      <h1>Hola estas en reservas</h1>
+      <div className={styles.portraitDiv}>
+        <h3>CORPOBELLO</h3>
+        <h2>TE ESTAMOS ESPERANDO</h2>
+      </div>
+      <Reserves />
     </main>
   );
 }
