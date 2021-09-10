f69f0bc922c460095489327c7bfe4af5f760a865
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-10 16:00:13 +0200

    Create pages portraits with customs photos and css

--- Corpobello/pages/contact.jsx
+++ Corpobello/pages/contact.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import styles from '../styles/Contact.module.css';
 
 export default function Contact() {
   return (
     <main>
+      <div className={styles.portraitDiv}>
+        <h3>CORPOBELLO</h3>
+        <h2>ENCUENTRANOS</h2>
+      </div>
       <h1>Hola estas en contacto</h1>
     </main>
   );
--- Corpobello/pages/events.jsx
+++ Corpobello/pages/events.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import styles from '../styles/Events.module.css';
 
 export default function Events() {
   return (
     <main>
+      <div className={styles.portraitDiv}>
+        <h3>VEN Y CONOCENOS</h3>
+        <h2>NUESTROS EVENTOS</h2>
+      </div>
       <h1>Hola estas en events</h1>
     </main>
   );
