c98cd40b15fd0d8129effd59d93ac265af2914b8
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-11 15:44:45 +0200

    Add contact information to contact page

--- Corpobello/pages/contact.jsx
+++ Corpobello/pages/contact.jsx
@@ -8,7 +8,29 @@ export default function Contact() {
         <h3>CORPOBELLO</h3>
         <h2>ENCUENTRANOS</h2>
       </div>
-      <h1>Hola estas en contacto</h1>
+      <div className={styles.contactContainer}>
+        <div>
+          <h3>CONTACTO</h3>
+          <h4>DIRECCIÓN:</h4>
+          <p>Calle Baldomer Sola 164</p>
+          <h4>TELÉFONO:</h4>
+          <p>935005616</p>
+          <h4>E- MAIL:</h4>
+          <p>corpobello@gmail.com</p>
+          <hr />
+          <h4>HORARIO DE TRABAJO:</h4>
+          <p>08:00 - 20:00</p>
+          <br />
+          <p className={styles.horary}>
+            De lunes a sábado
+            <br />
+            excepto festivos
+          </p>
+        </div>
+        <div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2990.808035871469!2d2.2325336159274776!3d41.443386779259065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4bb689555eb33%3A0x785edbf7358e869a!2sCorpo%20Bello!5e0!3m2!1sca!2ses!4v1631268488885!5m2!1sca!2ses" frameBorder="0" width="800" height="450" allowFullScreen="" title="contactMap" loading="lazy" />
+        </div>
+      </div>
     </main>
   );
 }
