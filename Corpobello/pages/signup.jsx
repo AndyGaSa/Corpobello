ba1a81a375371d9acad3a2cefb976e34d1c0e9c9
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-12 10:51:32 +0200

    Add name to signup page and make it validate forms and then send post

--- Corpobello/pages/signup.jsx
+++ Corpobello/pages/signup.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
   TextField,
   Button,
 } from '@material-ui/core';
-
 import styles from '../styles/Login.module.css';
 
 export default function Login() {
-  const [userTitle, setUserTitle] = useState('');
-  const [legend, setLegend] = useState('');
-  const [errorTitle, setErrorTitle] = useState(false);
+  const [sendClick, setClicked] = useState(1);
+  const [password, setPassword] = useState('');
+  const [legend, setPasswordLegend] = useState('');
+  const [errorTitle, setPasswordErrorTitle] = useState(false);
+  const [name, setName] = useState('');
+  const [nameLegend, setNameLegend] = useState('');
+  const [nameErrorTitle, setNameErrorTitle] = useState(false);
   const [mailTitle, setMailTitle] = useState('');
   const [mailLegend, setMailLegend] = useState('');
   const [mailErrorTitle, setMailErrorTitle] = useState(false);
@@ -18,68 +22,118 @@ export default function Login() {
     const mailRegexp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return mailRegexp.test(email);
   }
-  function checkValidation() {
+
+  async function checkValidation() {
     if (mailTitle.trim().length < 1) {
-      setMailErrorTitle(true);
-      setMailLegend('Email can\'t be empty');
+      await setMailErrorTitle(true);
+      setMailLegend('El E-mail no puede estar vacio!');
     } else if (!validateEmail(mailTitle)) {
-      setMailErrorTitle(true);
-      setMailLegend('Email not valid');
+      await setMailErrorTitle(true);
+      setMailLegend('Vaya! Parece que tu email no es valido...');
     } else {
-      setMailErrorTitle(false);
+      await setMailErrorTitle(false);
       setMailLegend('');
     }
 
-    if (userTitle.trim().length < 1) {
-      setErrorTitle(true);
-      setLegend('Username can\'t be empty');
+    if (password.trim().length < 1) {
+      await setPasswordErrorTitle(true);
+      setPasswordLegend('La contraseña  no puede estar vacia!');
     } else {
-      setErrorTitle(false);
-      setLegend('');
+      await setPasswordErrorTitle(false);
+      setPasswordLegend('');
     }
+
+    if (name.trim().length < 1) {
+      await setNameErrorTitle(true);
+      setNameLegend('El campo nombre no puede estar vacio!');
+    } else {
+      await setNameErrorTitle(false);
+      setNameLegend('');
+    }
+    setClicked(sendClick + 1);
   }
+  useEffect(() => {
+    (async function signup() {
+      if (errorTitle === false && nameErrorTitle === false && mailErrorTitle === false
+       && validateEmail(mailTitle) === true) {
+        const data = await axios.post('http://localhost:3000/api/signup', {
+          name,
+          email: mailTitle,
+          password,
+        });
+        alert(data);
+      }
+    }());
+  }, [sendClick]);
   return (
     <main className={styles.mainContainer}>
-      <section className={styles.loginContainer}>
-        <h1>REGISTRATE</h1>
-        <form className={styles.UserForm__UserMail}>
-          <TextField
-            onChange={(event) => {
-              setUserTitle(event.target.value);
-              if (userTitle.length > 12) {
-                setErrorTitle(true);
-                setLegend('Can\'t be longer than 12 characters');
-              } else {
-                setErrorTitle(false);
-                setLegend('');
-              }
-            }}
-            error={errorTitle}
-            label="Nombre"
-            helperText={legend}
-            variant="outlined"
-          />
-          <TextField
-            onChange={(event) => {
-              setMailTitle(event.target.value);
-              if (mailTitle.length > 24) {
-                setMailErrorTitle(true);
-                setMailLegend('Can\'t be longer than 24 characters');
-              } else {
-                setMailErrorTitle(false);
-                setMailLegend('');
-              }
-            }}
-            error={mailErrorTitle}
-            label="E-mail"
-            helperText={mailLegend}
-            variant="outlined"
-          />
-          <Button variant="contained" onClick={checkValidation} color="primary">
-            Login
-          </Button>
-        </form>
-      </section>
+      <div className={styles.bgDiv}>
+        <section className={styles.loginContainer}>
+          <h1>
+            Bienvenid@
+            <br />
+            Registrate y unete!
+          </h1>
+          <form className={styles.form}>
+            <TextField
+              className={styles.loginInput}
+              onChange={(event) => {
+                setName(event.target.value);
+                if (name.length > 24) {
+                  setNameErrorTitle(true);
+                  setNameLegend('El campo nombre no puede ser mas largo de 24 letras...');
+                } else {
+                  setNameErrorTitle(false);
+                  setNameLegend('');
+                }
+              }}
+              autoFocus="true"
+              error={nameErrorTitle}
+              label="Nombre y Apellidos"
+              helperText={nameLegend}
+              variant="outlined"
+            />
+            <TextField
+              className={styles.loginInput}
+              onChange={(event) => {
+                setMailTitle(event.target.value);
+                if (mailTitle.length > 50) {
+                  setMailErrorTitle(true);
+                  setMailLegend('El campo e-mail no puede ser mas largo de 50 caracteres!');
+                } else {
+                  setMailErrorTitle(false);
+                  setMailLegend('');
+                }
+              }}
+              error={mailErrorTitle}
+              label="E-mail"
+              helperText={mailLegend}
+              variant="outlined"
+            />
+            <TextField
+              className={styles.loginInput}
+              onChange={(event) => {
+                setPassword(event.target.value);
+                if (password.length > 30) {
+                  setPasswordErrorTitle(true);
+                  setPasswordLegend('La contraseña no puede ser mas larga de 30 caracteres!');
+                } else {
+                  setPasswordErrorTitle(false);
+                  setPasswordLegend('');
+                }
+              }}
+              error={errorTitle}
+              label="Contraseña"
+              type="password"
+              helperText={legend}
+              variant="outlined"
+            />
+            <Button variant="contained" onClick={() => checkValidation()}>
+              estoy list@
+            </Button>
+          </form>
+        </section>
+      </div>
     </main>
 
   );
