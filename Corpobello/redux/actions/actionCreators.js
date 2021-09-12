ee1fe64d679ea6201d014f4fa035b97eef156ff4
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-12 23:49:49 +0200

    Delete login function of actionCreators

--- Corpobello/redux/actions/actionCreators.js
+++ Corpobello/redux/actions/actionCreators.js
@@ -1,37 +1,7 @@
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
-export function login(name) {
-  return async (dispatch) => {
-    try {
-      const { data } = await axios.post('/api/login', { name });
-      return dispatch({
-        type: actionTypes.AUTH_LOGIN,
-        user: data,
-      });
-    } catch (error) {
-      if (error?.response?.status === 401) {
-        return dispatch({
-          type: actionTypes.AUTH_LOGIN_ERROR,
-          message: error.message,
-        });
-      }
-
-      return dispatch({
-        type: actionTypes.ERROR_GENERIC,
-        message: error.message,
-      });
-    }
-  };
-}
-
-export function logout() {
-  return {
-    type: actionTypes.AUTH_LOGOUT,
-  };
-}
-
-export function loadReserves() {
+export default function loadReserves() {
   return async (dispatch) => {
     const { data } = await axios.get('http://localhost:3000/api/reserveHandler');
     dispatch({
