f4bcd6b096d36701e80d477cfa80408276825280
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-13 22:55:00 +0200

    Fix validations and send update and logout to the api

--- Corpobello/components/UserForm.jsx
+++ Corpobello/components/UserForm.jsx
@@ -1,4 +1,8 @@
-import React, { useState } from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect } from 'react';
+import Notiflix from 'notiflix';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 import {
   FormHelperText,
   TextField,
@@ -9,25 +13,36 @@ import {
   FormControl,
   FormLabel,
   Button,
+  Avatar, IconButton,
 } from '@material-ui/core';
-
+import EditIcon from '@material-ui/icons/Edit';
 import styles from '../styles/UserForm.module.css';
 
-export default function UserForm() {
-  const [userTitle, setUserTitle] = useState('');
+export default function UserForm({ user }) {
+  const router = useRouter();
+  const [userTitle, setUserTitle] = useState(user?.name);
   const [legend, setLegend] = useState('');
+  const [sendClick, setClicked] = useState(1);
   const [errorTitle, setErrorTitle] = useState(false);
-  const [mailTitle, setMailTitle] = useState('');
+  const [mailTitle, setMailTitle] = useState(user?.email);
   const [mailLegend, setMailLegend] = useState('');
   const [mailErrorTitle, setMailErrorTitle] = useState(false);
   const [RadioValue, setRadioValue] = React.useState('');
   const [RadioErrorText, setRadioErrorText] = React.useState('');
   const [RadioError, setRadioError] = React.useState(false);
+  const [profileImg, setImg] = useState(user?.image);
+  const [description, setDescription] = useState(user?.description);
+  function onImageChange(event) {
+    if (event.target.files && event.target.files[0]) {
+      setImg(URL.createObjectURL(event.target.files[0]));
+    }
+  }
 
+  const descriptionPlaceholder = user && user?.description?.length > 0 ? user?.description : 'Comentanos aqui lo que necesites!';
   const RadioHandleChange = (event) => {
     setRadioValue(event.target.value);
     if (event.target.value.length < 1) {
-      setRadioErrorText('Gender is required');
+      setRadioErrorText('Necesitamos que rellenes este campo');
       setRadioError(true);
     } else {
       setRadioErrorText('');
@@ -41,10 +56,10 @@ export default function UserForm() {
   function checkValidation() {
     if (mailTitle.trim().length < 1) {
       setMailErrorTitle(true);
-      setMailLegend('Email can\'t be empty');
+      setMailLegend('El campo email no puede estar vacio');
     } else if (!validateEmail(mailTitle)) {
       setMailErrorTitle(true);
-      setMailLegend('Email not valid');
+      setMailLegend('Parece que el email que has introducido no es valido...');
     } else {
       setMailErrorTitle(false);
       setMailLegend('');
@@ -52,74 +67,130 @@ export default function UserForm() {
 
     if (userTitle.trim().length < 1) {
       setErrorTitle(true);
-      setLegend('Username can\'t be empty');
+      setLegend('El campo nombre no puede estar vacio');
     } else {
       setErrorTitle(false);
       setLegend('');
     }
 
     if (RadioValue.length < 1) {
-      setRadioErrorText('Gender is required');
+      setRadioErrorText('El campo genero es obligatorio');
       setRadioError(true);
     } else {
       setRadioErrorText('');
       setRadioError(false);
     }
+
+    setClicked(sendClick + 1);
   }
+  async function logout() {
+    try {
+      await axios.get('http://localhost:3000/api/logout');
+      router.push('/');
+    } catch (error) {
+      Notiflix.Report.failure('Error!', { error }, 'Ok');
+    }
+  }
+  useEffect(() => {
+    (async function signupAndLogin() {
+      try {
+        if (errorTitle === false && errorTitle === false && mailErrorTitle === false
+         && validateEmail(mailTitle) === true && sendClick > 1) {
+          await axios.put('http://localhost:3000/api/userHandler', {
+            name: userTitle,
+            email: mailTitle.toLowerCase(),
+            gender: RadioValue,
+            image: profileImg,
+            description,
+          });
+          Notiflix.Report.success('Guardado!', 'Se han guardado tus cambios con exito!', 'Ok');
+        }
+      } catch (error) {
+        Notiflix.Report.failure('Error!', { error }, 'Ok');
+      }
+    }());
+  }, [sendClick]);
   return (
-    <form className={styles.UserForm} data-testid="userForm">
-      <div className={styles.UserForm__UserMail}>
-        <TextField
-          onChange={(event) => {
-            setUserTitle(event.target.value);
-            if (userTitle.length > 12) {
-              setErrorTitle(true);
-              setLegend('Can\'t be longer than 12 characters');
-            } else {
-              setErrorTitle(false);
-              setLegend('');
-            }
-          }}
-          error={errorTitle}
-          label="Username"
-          helperText={legend}
-          variant="outlined"
-        />
-        <TextField
-          onChange={(event) => {
-            setMailTitle(event.target.value);
-            if (mailTitle.length > 24) {
-              setMailErrorTitle(true);
-              setMailLegend('Can\'t be longer than 24 characters');
-            } else {
-              setMailErrorTitle(false);
-              setMailLegend('');
-            }
-          }}
-          error={mailErrorTitle}
-          label="Email"
-          helperText={mailLegend}
-          variant="outlined"
-        />
-      </div>
-      <FormControl component="fieldset" error={RadioError}>
-        <FormLabel className={styles.RadioFormLabel} component="legend">Gender</FormLabel>
-        <RadioGroup aria-label="gender" id="radioButtons" name="gender1" value={RadioValue} onChange={RadioHandleChange}>
-          <FormControlLabel value="female" control={<Radio />} label="Female" />
-          <FormControlLabel value="male" control={<Radio />} label="Male" />
-          <FormHelperText>{RadioErrorText}</FormHelperText>
-        </RadioGroup>
-      </FormControl>
-      <div>
-        <div htmlFor="Bio" className={styles.BioForm}>
-          <span>Bio</span>
-          <TextareaAutosize aria-label="empty textarea" placeholder="King of The North" className={styles.TextArea} />
+    <>
+      <div className={styles.PortraitImg}>
+        <div className="UserAvatarDiv">
+          <Avatar alt="Jhon Snow" src={profileImg} className={styles.UserAvatarImg} />
+          <div />
+          <label htmlFor="icon-button-file" className={styles.editIconDiv}>
+            <input accept="image/*" className={styles.inputUpload} onChange={onImageChange} id="icon-button-file" type="file" />
+            <IconButton aria-label="upload picture" className={styles.iconButton} component="span">
+              <EditIcon style={{ color: 'white' }} />
+            </IconButton>
+          </label>
         </div>
       </div>
-      <Button variant="contained" onClick={() => checkValidation()} data-testid="saveButton" color="primary">
-        SAVE
-      </Button>
-    </form>
+      <form className={styles.UserForm} data-testid="userForm">
+        <div className={styles.UserForm__UserMail}>
+          <TextField
+            className={styles.formInputs}
+            onChange={(event) => {
+              setUserTitle(event.target.value);
+              if (userTitle.length > 30) {
+                setErrorTitle(true);
+                setLegend('El campo nombre no puede ser mas grande de 30 caracteres');
+              } else {
+                setErrorTitle(false);
+                setLegend('');
+              }
+            }}
+            error={errorTitle}
+            value={userTitle}
+            label="Nombre y Apellidos"
+            helperText={legend}
+            variant="outlined"
+          />
+          <TextField
+            className={styles.formInputs}
+            onChange={(event) => {
+              setMailTitle(event.target.value);
+              if (mailTitle.length > 30) {
+                setMailErrorTitle(true);
+                setMailLegend('El campo email no puede ser mas largo de 30 caracteres');
+              } else {
+                setMailErrorTitle(false);
+                setMailLegend('');
+              }
+            }}
+            value={mailTitle}
+            error={mailErrorTitle}
+            label="Email"
+            helperText={mailLegend}
+            variant="outlined"
+          />
+        </div>
+        <FormControl component="fieldset" error={RadioError}>
+          <FormLabel id={styles.RadioFormLabel} component="legend">Genero</FormLabel>
+          <RadioGroup aria-label="gender" id={styles.radioButtons} name="gender1" value={RadioValue} onChange={RadioHandleChange}>
+            <FormControlLabel className={styles.radioValues} value="female" control={<Radio />} label="Mujer" />
+            <FormControlLabel className={styles.radioValues} value="male" control={<Radio />} label="Hombre" />
+            <FormControlLabel className={styles.radioValues} value="unknown" control={<Radio />} label="Prefiero no decirlo" />
+            <FormHelperText>{RadioErrorText}</FormHelperText>
+          </RadioGroup>
+        </FormControl>
+        <div>
+          <div htmlFor="Bio" className={styles.BioForm}>
+            <span>Descripción - Comentarios</span>
+            <TextareaAutosize
+              aria-label="empty textarea"
+              placeholder={descriptionPlaceholder}
+              className={styles.TextArea}
+              onChange={(event) => setDescription(event.target.value)}
+            />
+          </div>
+        </div>
+        <Button className={styles.formButton} variant="contained" onClick={() => checkValidation()} data-testid="saveButton">
+          GUARDAR CAMBIOS
+        </Button>
+        <Button className={styles.logoutButton} variant="contained" onClick={() => logout()} data-testid="saveButton">
+          CERRAR SESIÓN
+        </Button>
+      </form>
+    </>
 
   );
 }
