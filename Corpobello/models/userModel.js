e34b32a20e5d219c92954aa28453659ed4ad70a8
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-13 22:54:17 +0200

    Add image, description and gender to user model

--- Corpobello/models/userModel.js
+++ Corpobello/models/userModel.js
@@ -15,6 +15,18 @@ const user = new Schema({
     type: String,
     required: true,
   },
+  image: {
+    type: String,
+    default: 'https://media.istockphoto.com/vectors/continuous-linear-drawing-of-female-face-line-art-vector-id1317825285?k=20&m=1317825285&s=612x612&w=0&h=1iVYGUaFRZJbGpLAOpebpjmkBWePovqy-f-J-RkFvbQ=',
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  gender: {
+    type: String,
+    default: 'unknown',
+  },
 });
 
 mongoose.models = {};
