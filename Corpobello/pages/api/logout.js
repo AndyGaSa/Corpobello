12d8dcf6718b1e279b41842ca02a5099e802ec63
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-14 13:34:02 +0200

    change switch statement to switch

--- Corpobello/pages/api/logout.js
+++ Corpobello/pages/api/logout.js
@@ -3,15 +3,11 @@ import {
   logout,
 } from '../../lib/controllers/userController';
 
-const logoutHandler = async (req, res) => {
-  switch (req.method) {
-    case 'GET':
-      await logout(req, res);
-      break;
-
-    default:
-      res.status(405).json({ message: 'This endpoint only support GET' });
-      break;
+const logoutHandler = (req, res) => {
+  if (req.method === 'GET') {
+    logout(req, res);
+  } else {
+    res.status(405).json({ message: 'This endpoint only support GET' });
   }
 };
 export default connectDB(logoutHandler);
