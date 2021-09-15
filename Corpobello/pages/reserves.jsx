03ad17fd5a328d1acd4a762f98353b9f9662f8a8
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-15 19:12:25 +0200

    Add head and proptypes to reserves page

--- Corpobello/pages/reserves.jsx
+++ Corpobello/pages/reserves.jsx
@@ -1,6 +1,7 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import Stripe from 'stripe';
+import { PropTypes } from 'prop-types';
+import Head from 'next/head';
 import Reserves from '../components/Reserves';
 import Header from '../components/Header';
 import styles from '../styles/Services.module.css';
@@ -8,6 +9,21 @@ import styles from '../styles/Services.module.css';
 export default function ReservesPage({ username, prices }) {
   return (
     <>
+      <Head>
+        <title>Corpobello -Reservas</title>
+        <meta name="description" content="Tu centro de estetica y peluqueria de confianza en Badalona" />
+        <link rel="icon" href="https://i.ibb.co/3Ryht66/Corpobello-Logo-Corto.png" />
+        <meta charset="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+        <meta name="keywords" content="Estetica Belleza Peluqueria Salon Masajes Masaje Spa Badalona" />
+
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link href="/static/images/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/static/images/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/static/images/apple-icon.png" />
+        <meta name="theme-color" content="#417505" />
+      </Head>
       <Header username={username} />
       <main>
         <div className={styles.portraitDiv}>
@@ -33,3 +49,8 @@ export async function getServerSideProps({ req }) {
     props: { username: req.cookies.username || 'undefined', prices: prices.data },
   };
 }
+
+ReservesPage.propTypes = {
+  username: PropTypes.string.isRequired,
+  prices: PropTypes.objectOf(PropTypes.string).isRequired,
+};
