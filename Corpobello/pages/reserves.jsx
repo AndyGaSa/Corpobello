d719f30c6c639c8b88a9263c1deef620d7b8cc73
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-15 12:01:26 +0200

    Get stripes props on reserve page

--- Corpobello/pages/reserves.jsx
+++ Corpobello/pages/reserves.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import Stripe from 'stripe';
 import Reserves from '../components/Reserves';
 import Header from '../components/Header';
 import styles from '../styles/Services.module.css';
 
-export default function ReservesPage({ username }) {
+export default function ReservesPage({ username, prices }) {
   return (
     <>
       <Header username={username} />
@@ -13,14 +14,22 @@ export default function ReservesPage({ username }) {
           <h3>CORPOBELLO</h3>
           <h2>TE ESTAMOS ESPERANDO</h2>
         </div>
-        <Reserves />
+        <Reserves prices={prices} />
       </main>
     </>
   );
 }
 
 export async function getServerSideProps({ req }) {
+  const stripe = new Stripe(process.env.STRIPE_RESTRICTED_KEY, {
+    apiVersion: '2020-08-27',
+  });
+  const prices = await stripe.prices.list({
+    active: true,
+    limit: 10,
+    expand: ['data.product'],
+  });
   return {
-    props: { username: req.cookies.username || 'undefined' },
+    props: { username: req.cookies.username || 'undefined', prices: prices.data },
   };
 }
