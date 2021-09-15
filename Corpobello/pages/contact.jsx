544261c4cbefcebb2d5a80b0a443163dc287c560
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-15 19:14:33 +0200

    Add head to contact page

--- Corpobello/pages/contact.jsx
+++ Corpobello/pages/contact.jsx
@@ -1,11 +1,27 @@
 import { PropTypes } from 'prop-types';
 import React from 'react';
+import Head from 'next/head';
 import Header from '../components/Header';
 import styles from '../styles/Contact.module.css';
 
 export default function Contact({ username }) {
   return (
     <>
+      <Head>
+        <title>Corpobello - Contacto</title>
+        <meta name="description" content="Tu centro de estetica y peluqueria de confianza en Badalona" />
+        <link rel="icon" href="https://i.ibb.co/3Ryht66/Corpobello-Logo-Corto.png" />
+        <meta charset="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+        <meta name="keywords" content="Estetica Belleza Peluqueria Salon Masajes Masaje Spa Badalona" />
+
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link href="/static/images/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/static/images/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/static/images/apple-icon.png" />
+        <meta name="theme-color" content="#417505" />
+      </Head>
       <Header username={username} />
       <main>
         <div className={styles.portraitDiv}>
