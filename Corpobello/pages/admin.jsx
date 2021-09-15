0a31bc83569a121dec896095b71a4d723f5c880a
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2021-09-15 23:32:59 +0200

    Add events to admin page and style them

--- Corpobello/pages/admin.jsx
+++ Corpobello/pages/admin.jsx
@@ -10,12 +10,21 @@ import stylesHome from '../styles/Home.module.css';
 import styles from '../styles/Events.module.css';
 import Header from '../components/Header';
 
-export default function Admin({ username, reserves }) {
+export default function Admin({ username, reserves, events }) {
   const router = useRouter();
   async function deleteReserve(reserveId) {
     try {
       await axios.delete('http://localhost:3000/api/reserveHandler', { data: { reserveId } });
       Notiflix.Report.success('Guardado!', 'Se ha borrado la reserva con exito!', 'Genial!');
+      router.reload(window.location.pathname);
+    } catch (error) {
+      Notiflix.Report.failure('Error!', { error }, 'Ok');
+    }
+  }
+  async function deleteEvent(eventId) {
+    try {
+      await axios.delete('http://localhost:3000/api/eventsHandler', { data: { eventId } });
+      Notiflix.Report.success('Guardado!', 'Se ha borrado el evento con exito!', 'Genial!');
       router.reload(window.location.pathname);
     } catch (error) {
       Notiflix.Report.failure('Error!', { error }, 'Ok');
@@ -39,10 +48,11 @@ export default function Admin({ username, reserves }) {
       <Header username={username} />
       <main>
         <div className={styles.portraitDiv}>
-          <h3>PANEL DE ADMINISTRACION</h3>
-          <h2>RESERVAS</h2>
+          <h3>CORPOBELLO</h3>
+          <h2>PANEL DE ADMINISTRACION</h2>
         </div>
         <section className={styles.ourServices}>
+          <h2>RESERVAS: </h2>
           <ul className={stylesHome.servicesCards}>
             {reserves?.map((reserve) => (
               <li key={reserve._id} className={styles.serviceCardOne}>
@@ -72,8 +82,26 @@ export default function Admin({ username, reserves }) {
                     {' '}
                     {reserve.personal}
                   </p>
-                  <Link href="/reserves">
+                  <Link href="/admin">
                     <a href="replace" onClick={() => deleteReserve(reserve._id)}> ELIMINAR RESERVA</a>
+                  </Link>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <hr />
+          <h2>EVENTOS:</h2>
+          <ul className={stylesHome.servicesCards}>
+            {events?.map((event) => (
+              <li key={event.title} className={styles.serviceCardTwo}>
+                <div>
+                  <h3>{event.title.toUpperCase()}</h3>
+                  <h4>{event.date}</h4>
+                  <p>
+                    {event.description}
+                  </p>
+                  <Link href="/admin">
+                    <a href="replace" onClick={() => deleteEvent(event._id)}> ELIMINAR EVENTO</a>
                   </Link>
                 </div>
               </li>
@@ -88,10 +116,12 @@ export default function Admin({ username, reserves }) {
 
 export async function getServerSideProps({ req }) {
   const { data } = await axios.get('http://localhost:3000/api/reserveHandler');
+  const events = await axios.get('http://localhost:3000/api/eventsHandler');
   return {
     props: {
       username: req.cookies.username || 'undefined',
       reserves: data,
+      events: events.data,
     },
   };
 }
@@ -99,4 +129,5 @@ export async function getServerSideProps({ req }) {
 Admin.propTypes = {
   username: PropTypes.string.isRequired,
   reserves: PropTypes.objectOf(PropTypes.string).isRequired,
+  events: PropTypes.objectOf(PropTypes.string).isRequired,
 };
